64733a4ac7a75ab995de93fc9ffd93baa8859e09
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2025-09-24 00:44:08 +0700

    Move all React components into code-cite.html
    All React component files and app.js were deleted and their code was consolidated into code-cite.html as inline scripts. This change simplifies deployment and maintenance by keeping the entire app in a single HTML file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		"./*.html", // Ini akan memindai code-cite.html dan index.html
+		"./components/**/*.html", // Ini akan memindai semua file .html di dalam folder components
+		"./app.js", // Ini akan memindai file app.js Anda
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
